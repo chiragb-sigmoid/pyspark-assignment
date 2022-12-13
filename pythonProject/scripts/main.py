c9424346aec55b88635d8dd18ca6ed554c257ca9
chirag bansal <chiragbansal@MacBook-Air.local>
2022-12-13 18:38:24 +0530

    Pyspark assignment

---
+++ pythonProject/scripts/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pyspark_files.pyspark_queries import sparkDataAnalysis
+from python_files.Python_apis import fetchData
+
+
+def testFunction():
+    # Test Function
+    print(f'Hi')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    testFunction()
+    # fetchData()
+    sparkDataAnalysis()
